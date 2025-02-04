14538837ddb18dce5340b9be87fa82ecd743396d
Mohammed Hinan A K <mohammedhinanak@Mohammeds-MacBook-Air.local>
2025-02-04 08:34:41 +0530

    Simplified next.config.mjs and confirmed local build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     unoptimized: true,
+  },
+  experimental: {
+    serverActions: true,
   },
 }
 export default nextConfig
